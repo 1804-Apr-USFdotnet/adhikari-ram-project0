d0040fc8defa73967f053c1a852458cbece77ba9
Ram Adhikari <ramadhikari96@gmail.com>
2018-04-23 11:26:14 -0400

    Palindrome done

---
+++ Palindrome/Palindrome/PalindromeCheck.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+namespace Palindrome
+{
+    public class PalindromeCheck
+    {
+        public bool IsPalindrome(string str)
+        {
+            string check = RemovePunctuation(str).ToLower();
+            if (check.Length <= 1)
+            {
+                return true;
+            }
+            else
+            {
+                int first = 0;
+                int last = check.Length - 1;
+                while (true)
+                {
+                    if (first > last)
+                    {
+                        return true;
+                    }
+                    char a = check[first];
+                    char b = check[last];
+                    if (a != b)
+                    {
+                        return false;
+                    }
+                    first++;
+                    last--;
+                }
+            }
+        }
+
+        public string RemovePunctuation(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in s)
+            {
+                if (!char.IsPunctuation(c))
+                    sb.Append(c);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Palindrome/PalindromeTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Palindrome;
+
+namespace PalindromeTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string test = "h";
+            string[] str = { "racecar", "Racecar", "1221", "never Odd, or Even.", "1231"};
+            bool expected = true;
+
+            PalindromeCheck checkP = new PalindromeCheck();
+            for(int i = 0; i < str.Length; i++)
+            {
+                bool actual = checkP.IsPalindrome(str[i]);
+                if (i == 4)
+                {
+                    expected = false;
+                }
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
